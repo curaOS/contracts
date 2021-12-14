979a9dd4e1e235b045b8c69b286dac20f130eb56
Yassine <yassinesania@gmail.com>
2021-12-14 13:20:26 +0100

    Add Account Token map to Persisten Tokens
    + implement supply for owner
    + rename persistent keys

--- as/cNFT/assembly/persistent_tokens.ts
+++ as/cNFT/assembly/persistent_tokens.ts
@@ -1,28 +1,49 @@
-import { PersistentUnorderedMap } from 'near-sdk-as'
-import { Token, AccountId } from './types'
+import { PersistentSet, PersistentUnorderedMap } from 'near-sdk-as'
+import { Token, AccountId, TokenId } from './types'
 
 @nearBindgen
 export class PersistentTokens {
-    private _map: PersistentUnorderedMap<AccountId, Token>
+    private _tmap: PersistentUnorderedMap<AccountId, Token>
+    private _amap: PersistentUnorderedMap<AccountId, PersistentSet<TokenId>>
 
     /**
      * @param prefix A prefix to use for every key of this map.
      */
     constructor(prefix: string) {
-        this._map = new PersistentUnorderedMap<AccountId, Token>(
-            '_map' + prefix
+        this._tmap = new PersistentUnorderedMap<AccountId, Token>(
+            '_tmap' + prefix
         )
+        this._amap = new PersistentUnorderedMap<
+            AccountId,
+            PersistentSet<TokenId>
+        >('_oset' + prefix)
     }
 
     /**
      * @param token_id ID of token to retrieve from _map.
      */
-    get(token_id: string): Token {
-        return this._map.getSome(token_id)
+    get(token_id: string): Token | null {
+        return this._tmap.get(token_id)
     }
 
+    /**
+     * Number of tokens stored in the contract.
+     */
     get number_of_tokens(): i32 {
-        return this._map.length
+        return this._tmap.length
+    }
+
+    /**
+     *
+     * @param account_id ID of account to retrieve supply.
+     * @returns string token supply of AccountId
+     */
+    supply_for_owner(account_id: string): string {
+        const accountMedia = this._amap.get(account_id)
+        if (accountMedia == null || accountMedia.size == 0) {
+            return '0'
+        }
+        return accountMedia.size.toString()
     }
 }
 
