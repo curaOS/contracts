6cb7cc19d8506bddbca50c99348232c3dd1de611
Yassine <yassinesania@gmail.com>
2021-12-18 11:59:26 +0100

    types.d.ts

--- as/cNFT/assembly/types.d.ts
+++ as/cNFT/assembly/types.d.ts
@@ -1,6 +1,12 @@
+/** TODO figure out how this can work with Token that has nearBindgen decorator */
 export declare class Token {
     id: string
     owner_id: string
+    creator: string
+    prev_owner: string
+    next_approval_id: number
+    media: string
+    extra: string
 }
 
 export declare type AccountId = string
