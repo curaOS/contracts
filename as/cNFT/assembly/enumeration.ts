216c43b18acb75ddc690bd777f6a9bdc17a4ba25
Yassine <yassinesania@gmail.com>
2021-12-18 11:59:13 +0100

    Enumeration nft file

--- as/cNFT/assembly/enumeration.ts
+++ as/cNFT/assembly/enumeration.ts
@@ -1,5 +1,9 @@
-import { persistent_tokens } from './persistent_tokens'
+import { persistent_tokens } from './models/persistent_tokens'
 
 export function nft_supply_for_owner(account_id: string): string {
     return persistent_tokens.supply_for_owner(account_id)
+}
+
+export function nft_total_supply(): string {
+    return persistent_tokens.number_of_tokens.toString()
 }
