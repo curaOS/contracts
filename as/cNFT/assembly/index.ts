65b7f885663e36172b60eba1402916d89920605e
Yassine <yassinesania@gmail.com>
2021-12-18 11:58:39 +0100

    Export functions from index

---
+++ as/cNFT/assembly/index.ts
@@ -0,0 +1,11 @@
+import { persistent_tokens } from './models/persistent_tokens'
+
+export { mint } from './mint'
+
+export { nft_token } from './core'
+
+export { nft_supply_for_owner, nft_total_supply } from './enumeration'
+
+export function init(): void {
+    return
+}
