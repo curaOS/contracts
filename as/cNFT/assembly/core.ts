49e259836bd286012d167d117faa8024b19ead3d
Yassine <yassinesania@gmail.com>
2021-12-23 15:23:42 +0100

    Add @nearBindgen to imported functions

--- as/cNFT/assembly/core.ts
+++ as/cNFT/assembly/core.ts
@@ -1,6 +1,7 @@
 import { Token } from './models/token'
 import { persistent_tokens } from './models/persistent_tokens'
 
+@nearBindgen
 export function nft_token(token_id: string): Token | null {
     return persistent_tokens.get(token_id)
 }
