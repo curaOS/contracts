f74586b639ab253fcfe88b7056a6938e26d0df04
achraf elghinoussi <afaithraf@gmail.com>
2021-12-29 17:56:19 +0100

    Created  `persistent_market`

--- as/cNFT/assembly/index.ts
+++ as/cNFT/assembly/index.ts
@@ -6,6 +6,8 @@ export { nft_token } from './core'
 
 export { nft_supply_for_owner, nft_total_supply, nft_tokens, nft_tokens_for_owner } from './enumeration'
 
+export { bid, get_bids, get_bidder_bids } from './market'
+
 export function init(): void {
     return
 }
