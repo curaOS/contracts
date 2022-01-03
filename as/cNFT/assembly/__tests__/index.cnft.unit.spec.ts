9e59a3a294e6bbcfa6c470710b300d25a911b82f
Yassine <yassinesania@gmail.com>
2022-01-03 16:55:16 +0100

    Test init and nft_metadata

--- as/cNFT/assembly/__tests__/index.cnft.unit.spec.ts
+++ as/cNFT/assembly/__tests__/index.cnft.unit.spec.ts
@@ -1,12 +1,15 @@
 import { VMContext } from 'near-mock-vm'
 import { VM, context, logging } from 'near-sdk-as'
 import { TokenMetadata } from '../../../NFT/assembly/metadata'
+import { NFTContractMetadata } from '../models/nft_contract_metadata'
 
 import {
     nft_total_supply,
     nft_token,
     nft_supply_for_owner,
     mint,
+    nft_metadata,
+    init,
 } from '../index'
 import { persistent_tokens } from '../models/persistent_tokens'
 import { Token } from '../models/token'
@@ -20,6 +23,12 @@ const mintToken = (accountId: AccountId): Token => {
     token_metadata.extra = 'extra'
     const token = mint(token_metadata)
     return token
+}
+
+const initContract = (): void => {
+    const nft_contract_metadata = new NFTContractMetadata()
+
+    init(nft_contract_metadata)
 }
 
 describe('- CONTRACT -', () => {
@@ -48,5 +57,13 @@ describe('- CONTRACT -', () => {
         const nftSupplyForOwner = nft_supply_for_owner('prova.testnet')
 
         log(nftSupplyForOwner)
+    })
+
+    it('xxx returns nft contract metadata', () => {
+        initContract()
+
+        const nftContractMetadata = nft_metadata()
+
+        log(nftContractMetadata)
     })
 })
