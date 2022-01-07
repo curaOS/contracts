3f7b8b99325232b9a48fb2c0a230ec26e2eaad9e
Yassine <yassinesania@gmail.com>
2022-01-07 16:34:07 +0100

    Comments

--- as/cNFT/assembly/mint.ts
+++ as/cNFT/assembly/mint.ts
@@ -1,32 +1,32 @@
 import { context, logging, u128 } from 'near-sdk-as'
 import { assert_deposit_attached } from './utils/asserts'
-import { Token } from './models/token'
+import { Token } from './models/persistent_tokens'
 import { persistent_tokens } from './models/persistent_tokens'
 import { persistent_tokens_metadata, TokenMetadata } from './models/persistent_tokens_metadata'
 
 @nearBindgen
 export function mint(tokenMetadata: TokenMetadata): Token {
-    /** TODO Assert deposit attached based on custom amount from init */
+    /**@todo Assert deposit attached based on custom amount from init */
     //     assert_deposit_attached(u128.from(0))
-
-    /** TODO Assert uniqueId is actually unique */
 
     let token = new Token()
 
-    token.media = tokenMetadata.media
-    token.extra = tokenMetadata.extra
-    /** TODO not always */
+    /**@todo Not always sender is creator i guess, would rather pass it to function */
     token.creator = context.sender
 
-    /**@todo generate valid token id */
+    /**
+     * @todo Assert uniqueId is actually unique
+     * @todo Generate valid token id */
     const tokenId = persistent_tokens.number_of_tokens.toString()
+
+    persistent_tokens_metadata.add(tokenId, tokenMetadata)
+
     persistent_tokens.add(
         tokenId,
         token,
         context.sender
     )
 
-    persistent_tokens_metadata.add(tokenId, tokenMetadata)
 
     return token
 }
