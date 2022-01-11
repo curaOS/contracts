4f5b2e86adf01828f400488517fb999bd17a3cd3
Yassine <yassinesania@gmail.com>
2022-01-11 14:51:13 +0100

    New types for Royalty

--- as/cNFT/assembly/types.d.ts
+++ as/cNFT/assembly/types.d.ts
@@ -1,2 +1,9 @@
+import { u128 } from 'near-sdk-as'
+
 export declare type AccountId = string
 export declare type TokenId = string
+
+export declare type Payout = Map<AccountId, u128>
+
+export declare type Amount = u128
+export declare type Balance = Amount
