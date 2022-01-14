a05c02d36019084ac6b6c2f9813533c2a251baf0
achraf elghinoussi <afaithraf@gmail.com>
2022-01-14 11:55:45 +0100

    fix `nft_tokens` and `nft_tokens_for_owner`

--- as/cNFT/assembly/enumeration.ts
+++ as/cNFT/assembly/enumeration.ts
@@ -1,5 +1,7 @@
+import { nft_token } from './core'
 import { persistent_tokens } from './models/persistent_tokens'
 import { Token } from './models/persistent_tokens'
+import { persistent_tokens_metadata } from './models/persistent_tokens_metadata'
 
 @nearBindgen
 export function nft_supply_for_owner(account_id: string): string {
@@ -14,17 +16,45 @@ export function nft_total_supply(): string {
 
 @nearBindgen
 export function nft_tokens(from_index: string = '0', limit: u8 = 0): Token[] {
+    // get an array of tokenId from tokens_metadata
+    const keys = persistent_tokens_metadata.keys()
+    
+    // first key
     const start = <u32>parseInt(from_index)
-    const end = <u32>(limit == 0 ? parseInt(nft_total_supply()) : limit) + start
+    // last key
+    const end = <u32>(limit == 0 ? keys.length : limit) + start
 
-    return persistent_tokens.tokens(start, end);
+    // empty token array
+    let tokens: Token[] = [];        
+
+    for (let i = start; i < end; i++) {
+        // get token and add it the tokens array
+        let token = nft_token(keys[i]);  
+        tokens.push(token)
+    }
+
+    return tokens;
 }
 
 
 @nearBindgen
 export function nft_tokens_for_owner(account_id: string, from_index: string = '0', limit: u8 = 0): Token[] {
+    // get an array of tokenId for owner
+    const keys =  persistent_tokens.tokens_for_owner(account_id);
+    
+    // first key
     const start = <u32>parseInt(from_index)
-    const end = <u32>(limit == 0 ? parseInt(nft_supply_for_owner(account_id)) : limit) + start
+    // last key
+    const end = <u32>(limit == 0 ? keys.length : limit) + start
 
-    return persistent_tokens.tokens_for_owner(account_id, start, end);
+    // empty token array
+    let tokens: Token[] = [];        
+
+    for (let i = start; i < end; i++) {
+        // get token and add it the tokens array
+        let token = nft_token(keys[i]);  
+        tokens.push(token)
+    }
+
+    return tokens;
 }
