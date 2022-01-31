f5ecc66e6c34f30de0bfd6b26d3247bc5f5014be
achraf elghinoussi <afaithraf@gmail.com>
2022-01-31 08:10:21 +0100

    assert `nft_per_owner` when minting

--- as/cNFT/assembly/mint.ts
+++ as/cNFT/assembly/mint.ts
@@ -5,11 +5,20 @@ import { persistent_tokens } from './models/persistent_tokens'
 import { persistent_tokens_metadata, TokenMetadata } from './models/persistent_tokens_metadata'
 import { persistent_tokens_royalty, TokenRoyalty } from './models/persistent_tokens_royalty'
 import { NftEventLogData, NftMintLog } from './models/log'
+import { nft_metadata } from './metadata'
 
 @nearBindgen
 export function mint(tokenMetadata: TokenMetadata, token_royalty: TokenRoyalty): Token {
     /**@todo Assert deposit attached based on custom amount from init */
     //     assert_deposit_attached(u128.from(0))
+
+
+    const owner_supply = parseInt(persistent_tokens.supply_for_owner(context.sender))
+    const nft_per_owner = nft_metadata().nft_per_owner
+    // if nft_per_owner is 0 then skip assert
+    if(nft_per_owner){
+        assert(owner_supply < nft_per_owner, `Limited to ${nft_per_owner} NFTs per owner`)
+    }
 
     let token = new Token()
 
