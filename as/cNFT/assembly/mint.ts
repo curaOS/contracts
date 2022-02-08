2fa7627e382c8cadcf97345d978b19a9c66bc7fe
Achraf <afaithraf@gmail.com>
2022-02-08 14:05:55 +0100

    Assert mint price

--- as/cNFT/assembly/mint.ts
+++ as/cNFT/assembly/mint.ts
@@ -1,15 +1,18 @@
-import { context, logging, u128 } from 'near-sdk-as'
+import { context, logging, storage, u128 } from 'near-sdk-as'
 import { assert_deposit_attached } from './utils/asserts'
 import { Token } from './models/persistent_tokens'
 import { persistent_tokens } from './models/persistent_tokens'
 import { persistent_tokens_metadata, TokenMetadata } from './models/persistent_tokens_metadata'
 import { persistent_tokens_royalty, TokenRoyalty } from './models/persistent_tokens_royalty'
 import { NftEventLogData, NftMintLog } from './models/log'
+import { NFTContractExtra, PersistentNFTContractMetadata } from './models/persistent_nft_contract_metadata'
 
 @nearBindgen
 export function mint(tokenMetadata: TokenMetadata, token_royalty: TokenRoyalty): Token {
-    /**@todo Assert deposit attached based on custom amount from init */
-    //     assert_deposit_attached(u128.from(0))
+
+    /** Assert deposit attached based on custom amount from init */
+    const contract_extra = storage.getSome<NFTContractExtra>(PersistentNFTContractMetadata.STORAGE_KEY_EXTRA)
+    assert_deposit_attached(u128.fromString(contract_extra.mint_price))
 
     let token = new Token()
 
@@ -33,16 +36,14 @@ export function mint(tokenMetadata: TokenMetadata, token_royalty: TokenRoyalty):
 
     persistent_tokens_royalty.add(tokenId, token_royalty)
 
-
-    
     // Immiting log event
     const mint_log = new NftMintLog()
-    
+
     mint_log.owner_id = context.sender
     mint_log.token_ids = [tokenId]
     mint_log.tokens = [token]
     mint_log.metadata = [tokenMetadata]
-    
+
     const log = new NftEventLogData<NftMintLog>("nft_mint", [mint_log])
 
     logging.log(log)
