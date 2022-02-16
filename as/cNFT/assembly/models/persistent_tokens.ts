b9bec5bcdb1849bbc3a1edee3e740e3fd5b5bdce
AshenI99 <asheniranda@Ashens-MacBook-Air.local>
2022-02-17 00:38:56 +0530

    Added persistent_token module docs

--- as/cNFT/assembly/models/persistent_tokens.ts
+++ as/cNFT/assembly/models/persistent_tokens.ts
@@ -34,40 +34,108 @@ export class PersistentTokens {
         this._oset = new PersistentSet<AccountId>('_oset' + prefix)
     }
 
+
+
     /**
+     * Get token details of a single token
+     *
+     * **Basic usage example:**
+     *
+     * Assume we need to get the details of the token with token id = `jenny911038`,
+     * ```
+     * const persistent_tokens = new PersistentTokens('pt')
+     * const token = persistent_tokens.get("jenny911038");
+     * ```
+     *
      * @param token_id ID of token to retrieve from _map
+     * @return Token details object
      */
     get(token_id: TokenId): Token {
         // getSome will throw an error if token_id doesn't exist
         return this._tmap.getSome(token_id)
     }
 
+
+
     /**
-     * @param token_id ID of token to check if exists
+     * Check if the relevant token exists or not
+     *
+     * **Basic usage example:**
+     *
+     * Assume we need to check if the token with token id = `jenny911038` present in the storage,
+     * ```
+     * const persistent_tokens = new PersistentTokens('pt')
+     * const is_token_exists = persistent_tokens.has("jenny911038");
+     * ```
+     *
+     * @param token_id ID of the token to check it's exists or not
+     * @return true if exits, false if not exists
      */
     has(token_id: TokenId): bool {
         return this._tmap.contains(token_id)
     }
 
+
+
     /**
-     * @returns an array of tokenId
+     * Get the IDs of tokens saved in the provided range
+     *
+     * **Basic usage example:**
+     *
+     * Assume we need the IDs of first 5 tokens,
+     * ```
+     * const persistent_tokens = new PersistentTokens('pt')
+     * const token_ids = persistent_tokens.keys(0, 5);
+     * ```
+     *
+     * @param start Start index
+     * @param end End index
+     * @return An array of tokenIds
      */
     keys(start: i32, end: i32): TokenId[] {
         return this._tmap.keys(start, end)
     }
 
     /**
-     * Number of tokens stored in the contract.
-     * TODO not sure _tmap.lenght can represent up to u128 values
+     * TODO not sure _tmap.length can represent up to u128 values
+     */
+
+    /**
+     * Get number of tokens saved in the contract
+     *
+     * **Basic usage example:**
+     *
+     * Assume contract has 9450 tokens,
+     *
+     * ```
+     * const persistent_tokens = new PersistentTokens('pt')
+     * const tokens_length = persistent_tokens.number_of_tokens();
+     * log(parseInt(tokens_length)) // 9450
+     * ```
+     *
+     * @return Number of tokens present in the contract in `u128` format
      */
     get number_of_tokens(): u128 {
         return u128.from(this._tmap.length)
     }
 
+
+
     /**
+     * Get number of tokens saved in the contract for a given account
      *
-     * @param accountId ID of account to retrieve supply
-     * @returns string token supply of AccountId
+     * **Basic usage example:**
+     *
+     * Assume user with account id = `alice.test.near` has 12 tokens in the contract,
+     *
+     * ```
+     * const persistent_tokens = new PersistentTokens('pt')
+     * const tokens_length_for_user = persistent_tokens.supply_for_owner("alice.test.near");
+     * log(tokens_length_for_user) // "12"
+     * ```
+     *
+     * @param accountId ID of the account to retrieve number of tokens that account owns
+     * @return Number of tokens present for the given account in the contract
      */
     supply_for_owner(accountId: AccountId): string {
         let accountTokenSet = this._amap.get(accountId)
@@ -79,20 +147,43 @@ export class PersistentTokens {
         return accountTokenSet.size.toString()
     }
 
+
+
     /**
-     * @param accountId ID of account to retrieve tokens for
-     * @returns an array of tokenId for owner
+     * Get the IDs of tokens saved in the contract for a given account/user
+     *
+     * **Basic usage example:**
+     *
+     * Assume we need the IDs of the tokens that user with account id = `alice.test.near` owns,
+     * ```
+     * const persistent_tokens = new PersistentTokens('pt')
+     * const token_ids_for_user = persistent_tokens.tokens_for_owner("alice.test.near");
+     * ```
+     *
+     * @param accountId ID of the account to retrieve token IDs of the tokens that account/user owns
+     * @return An array of tokenIds that the given account/user owns
      */
     tokens_for_owner(accountId: AccountId): TokenId[] {
         return this._amap.getSome(accountId).values()
     }
 
+
+
     /**
+     * Add a new token to the contract
      *
-     * @param tokenId Id of new token
-     * @param token Token to be added
-     * @param accountId Owner of the token
-     * @returns added token
+     * **Basic usage example:**
+     *
+     * Assume we need add the token `T1` to the contract, and it owns by the user with account id = `alice.test.near`,
+     * ```
+     * const persistent_tokens = new PersistentTokens('pt')
+     * const token = persistent_tokens.add('alice_23456' , T1 , "alice.test.near");
+     * ```
+     *
+     * @param tokenId Id of the new token
+     * @param token Token to be saved to the contract
+     * @param accountId ID of the account that owns the token
+     * @return Saved token
      */
     add(tokenId: TokenId, token: Token, accountId: AccountId): Token {
         this._tmap.set(tokenId, token)
@@ -108,10 +199,20 @@ export class PersistentTokens {
     }
 
 
+
     /**
-     * Burn a token
-     * @param tokenId: Id of the token to remove
-     * @param accountId: Account of token to remove
+     * Burn a token from the contract
+     *
+     * **Basic usage example:**
+     *
+     * Assume we need burn the token with token id = `alice_23456` from the contract,
+     * ```
+     * const persistent_tokens = new PersistentTokens('pt')
+     * persistent_tokens.burn('alice_23456' , "alice.test.near");
+     * ```
+     *
+     * @param tokenId Id of the token to burn
+     * @param accountId ID of the account that owns the burning token
      */
     burn(tokenId: TokenId, accountId: AccountId): void {
         let token = this._tmap.getSome(tokenId);
@@ -122,10 +223,21 @@ export class PersistentTokens {
         this.remove(tokenId, accountId);
     }
 
+
+
     /**
-     * Remove a token from a user's token set.
-     * @param tokenId: Id of the token to remove
-     * @param accountId: Account of token to remove
+     * Remove a token from a user's token set
+     *
+     * **Basic usage example:**
+     *
+     * Assume we need remove the token with token id = `alice_23456` from the user with account id = `alice.test.near`,
+     * ```
+     * const persistent_tokens = new PersistentTokens('pt')
+     * persistent_tokens.remove('alice_23456' , "alice.test.near");
+     * ```
+     *
+     * @param tokenId Id of the token to remove
+     * @param accountId ID of the account that owns the removing token
      */
     remove(tokenId: TokenId, accountId: AccountId): void {
 
