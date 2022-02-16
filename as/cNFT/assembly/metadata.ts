8e02658e90f69eb6183c1ebe697717466e4b1b96
AshenI99 <asheniranda@Ashens-MacBook-Air.local>
2022-02-16 11:59:17 +0530

    Added metadata module docs

--- as/cNFT/assembly/metadata.ts
+++ as/cNFT/assembly/metadata.ts
@@ -2,6 +2,18 @@ import { storage } from 'near-sdk-as'
 import { PersistentNFTContractMetadata, NFTContractMetadata } from './models/persistent_nft_contract_metadata'
 
 
+/**
+ * Get NFT Metadata of the contract.
+ *
+ *
+ * **Basic usage example:**
+ *
+ * ```
+ * const meta = nft_metadata();
+ * ```
+ *
+ * @return Metadata details of the NFT Contract
+ */
 @nearBindgen
 export function nft_metadata(): NFTContractMetadata {
        return storage.getSome<NFTContractMetadata>(PersistentNFTContractMetadata.STORAGE_KEY_STANDARD)
