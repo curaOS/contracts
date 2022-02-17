17c2da88ecde278af89299f1ca3d38baff3a8fc6
AshenI99 <asheniranda@Ashens-MacBook-Air.local>
2022-02-17 20:09:23 +0530

    Added persistent_token_royalty module docs

--- as/cNFT/assembly/models/persistent_tokens_royalty.ts
+++ as/cNFT/assembly/models/persistent_tokens_royalty.ts
@@ -3,10 +3,18 @@ import { TokenId, AccountId } from '../types'
 
 @nearBindgen
 export class TokenRoyalty {
+
+    /** Map of Account IDs and their royalty values to the respective token  */
     split_between: Map<AccountId, u32>
+
+    /** Percentage of the balance to be divided to the royalties */
     percentage: u32
 }
 
+
+/**
+ * @hidden
+ */
 export function defaultTokenRoyalty(): TokenRoyalty {
     return {
         split_between: new Map(),
@@ -28,11 +36,21 @@ export class PeristentTokenRoyalty {
         )
     }
 
+
     /**
+     * Add royalty details of a token
      *
-     * @param tokenId Id of token
-     * @param tokenRoyalty TokenRoyalty to be added
-     * @returns added TokenRoyalty
+     * **Basic usage example:**
+     *
+     * Assume we need add the royalty object `TR1` to the token with the token id = `jenny911038`,
+     * ```
+     * const persistent_tokens_royalty = new PeristentTokenRoyalty('ptr');
+     * const token_royalty = persistent_tokens_royalty.add('jenny911038' , TR1 );
+     * ```
+     *
+     * @param tokenId Id of the token
+     * @param tokenRoyalty Royalty object of the token to be added
+     * @return Saved royalty object of the token
      */
     add(tokenId: TokenId, tokenRoyalty: TokenRoyalty): TokenRoyalty {
         this._trmap.set(tokenId, tokenRoyalty)
@@ -40,8 +58,20 @@ export class PeristentTokenRoyalty {
         return tokenRoyalty
     }
 
+
     /**
-     * @param token_id ID of token to retrieve from _map
+     * Get token royalty for a given token ID
+     *
+     * **Basic usage example:**
+     *
+     * Assume we need to get the royalty of the token with token id = `jenny911038`,
+     * ```
+     * const persistent_tokens_royalty = new PeristentTokenRoyalty('ptr');
+     * const token_royalty = persistent_tokens_royalty.get("jenny911038");
+     * ```
+     *
+     * @param token_id ID of token to retrieve token royalty details
+     * @return Token Royalty object if exists, otherwise `null`
      */
     get(token_id: TokenId): TokenRoyalty | null {
         return this._trmap.get(token_id)
