325857a071287e06d678a4d50cd1f974be6d3582
AshenI99 <asheniranda@Ashens-MacBook-Air.local>
2022-02-18 17:57:00 +0530

    Added utils/royalties  module docs

--- as/cNFT/assembly/utils/royalties.ts
+++ as/cNFT/assembly/utils/royalties.ts
@@ -1,13 +1,18 @@
 import { u128 } from 'near-sdk-as'
 import { Balance } from '../types'
 
+/** @hidden */
 const ONE_HUNDRED_PERCENT: u16 = 10000
 
-/**
- * @todo describle function
- * Royalty payout
- */
 
+/**
+ * Get the amount that need to pay for a royalty account when the token value and royalty percentage is given. Return value is in `u128` format.
+ *
+ *
+ * @param royalty_percentage Percentage of the token value, that need to pay for the royalty account
+ * @param amount_to_pay Value of the token
+ * @return Royalty amount to pay for the account in `u128` format
+ */
 export function royalty_to_payout(
     royalty_percentage: u32,
     amount_to_pay: Balance
