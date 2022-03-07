f8ea549ed486c52680f050b72573a39a2d80ae4e
AshenI99 <asheniranda@Ashens-MacBook-Air.local>
2022-03-07 19:12:09 +0530

    token metadata validation

--- as/utils.ts
+++ as/utils.ts
@@ -117,3 +117,9 @@ export function royalty_to_payout(
         u128.from(ONE_HUNDRED_PERCENT)
     )
 }
+
+
+/** @hidden */
+export function randomInt(min: number = 0, max: number = 10): number {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
