c1df0f4aac3356052e96e578c43ba6ec2a8ccbbc
Yassine <yassinesania@gmail.com>
2022-07-01 15:49:06 +0200

    Revert market tests to sandbox

--- near-workspaces/__tests__/market.ava.ts
+++ near-workspaces/__tests__/market.ava.ts
@@ -17,7 +17,7 @@ import {
 import { formatNearAmount } from 'near-api-js/lib/utils/format'
 
 const workspace = Workspace.init(
-    { network: 'testnet', rootAccount: 'bscs-x.testnet' },
+    // { network: 'testnet', rootAccount: '[account].testnet' },
     async ({ root }) => ({
         contract: await root.createAndDeploy(
             'cnft',
